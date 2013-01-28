f45f0bd2fbc4bf523b924f672303ee9bb72a9cc9
Francesco Canessa <makevoid@gmail.com>
2013-01-29 00:06:57 +0100

    temp update

--- s3play.js
+++ s3play.js
@@ -325,10 +325,10 @@ var S3Play = Em.Object.create({
 
     // TODO: scan markers (this is very manual)
     var a = this.get_one(null)
-    var b = this.get_one("Children Of Bodom")
-    var c = this.get_one("Tsueno Imahori")
-
-    $.when(a, b, c).done(function(){
+    var b = this.get_one("Cat Power")
+    var c = this.get_one("Lele")
+    var d = this.get_one("Tool")
+    $.when(a, b, c, d).done(function(){
       callback()
     })
   },
